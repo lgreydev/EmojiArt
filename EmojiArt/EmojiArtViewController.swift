dace63c743fcca3dee867ebdc68584831b1cd6c5
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-06-13 17:24:21 +0300

    Remove storyboard

---
+++ EmojiArt/EmojiArtViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EmojiArtViewController.swift
+//  EmojiArt
+//
+//  Created by Sergey Lukaschuk on 13.06.2022.
+//
+
+import UIKit
+
+class EmojiArtViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = . red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
