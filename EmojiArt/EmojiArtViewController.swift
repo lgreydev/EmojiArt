a0c1163ee0de9402216a914a426514d08e7bcb8b
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-07-19 13:11:04 +0300

    Setup interactions

--- EmojiArt/EmojiArtViewController.swift
+++ EmojiArt/EmojiArtViewController.swift
@@ -10,66 +10,34 @@ import TinyConstraints
 
 class EmojiArtViewController: UIViewController, UIDropInteractionDelegate {
 
-    private var dropZone = UIView() {
+    @IBOutlet weak var dropZone: UIView! {
         didSet {
             dropZone.addInteraction(UIDropInteraction(delegate: self))
         }
     }
 
-    private lazy var emojiArtView: EmojiArtView = {
-        let view = EmojiArtView()
-        return view
-    }()
+    @IBOutlet weak var emojiArtView: EmojiArtView!
 
-    var imageFetcher: ImageFetcher!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        addViews()
-        constraintsToViews()
-        emojiArtView.backgroundImage = UIImage(named: "142774")
-        print(#function)
-    }
-
-    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
-        print(#function)
-        return session.canLoadObjects(ofClass: NSURL.self) && session.canLoadObjects(ofClass: UIImage.self)
-    }
-
-    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
-        print(#function)
-        return UIDropProposal(operation: .copy)
-    }
-
-    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
-        print(#function)
-        imageFetcher = ImageFetcher() { (url, image) in
-            DispatchQueue.main.async {
-                self.emojiArtView.backgroundImage = image
-            }
-        }
-
-        session.loadObjects(ofClass: NSURL.self) { nsurl in
-            guard let url = nsurl.first as? URL else { return }
-            self.imageFetcher.fetch(url)
-        }
-
-        session.loadObjects(ofClass: UIImage.self) { images in
-            guard let image = images.first as? UIImage else { return }
-            self.imageFetcher.backup = image
-        }
-    }
 }
 
 
 extension EmojiArtViewController {
-    private func addViews() {
-        view.addSubview(dropZone)
-        view.addSubview(emojiArtView)
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: NSURL.self) && session.canLoadObjects(ofClass: UIImage.self)
     }
 
-    private func constraintsToViews() {
-        dropZone.edgesToSuperview()
-        emojiArtView.edgesToSuperview()
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
+    }
+
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        session.loadObjects(ofClass: NSURL.self) { nsurls in
+            <#code#>
+        }
+
+        session.loadObjects(ofClass: UIImage.self) { images in
+            <#code#>
+        }
     }
 }
+
